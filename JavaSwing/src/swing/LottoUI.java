6e85172bc6921b2a1981fad0c15bb9736974933d
Hoonl_2 <Hoonl_2@Hoonl-PC>
2015-07-30 15:22:39 +0900

    2015.07.30 수강종료

---
+++ JavaSwing/src/swing/LottoUI.java
@@ -0,0 +1,20 @@
+package swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+// UI : User Interface 사용자 화면
+public class LottoUI extends JFrame implements ActionListener{
+	public LottoUI() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
